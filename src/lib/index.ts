9fad179568a00700bd424289e102bf40e41920b7
Maximilian <github@maximilian-heidenreich.de>
2023-09-07 00:58:50 +0200

    fix exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,22 +1,28 @@
-import Board from "./Board.svelte";
-import Positionable from "./Positionable.svelte";
-import Draggable from "./Draggable.svelte";
-import Resizable from "./Resizable.svelte";
-import Grid from "./Grid.svelte";
-import type { TBoard, TBoardSettings } from "./types/Board.type.js";
-import type { Vec2 } from "./types/Utils.type.js";
+// import Board from "./Board.svelte";
+// export default { Board };
+
+export { default as Board } from "./Board.svelte";
+export { default as Positionable } from "./Positionable.svelte";
+export { default as Draggable } from "./Draggable.svelte";
+export { default as Resizable } from "./Resizable.svelte";
+export { default as Grid } from "./Grid.svelte";
+
+export type { TBoard, TBoardSettings } from "./types/Board.type.js";
+export type { Vec2 } from "./types/Utils.type.js";
+
 
 // Reexport your entry components here
-export default {
-  Board,
-  Positionable,
-  Draggable,
-  Resizable,
-  Grid
-};
 
-export type {
-  TBoardSettings,
-  TBoard,
-  Vec2
-}
+// export default {
+//   Board,
+//   Positionable,
+//   Draggable,
+//   Resizable,
+//   Grid
+// };
+
+// export type {
+//   TBoardSettings,
+//   TBoard,
+//   Vec2
+// }
