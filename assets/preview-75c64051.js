2842bdb82ba4d0d463f0e39c3d1de93a43c57453
Maximilian <github@maximilian-heidenreich.de>
2023-09-15 16:28:37 +0200

    Updates

---
+++ assets/preview-75c64051.js
@@ -0,0 +1,2 @@
+const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Getting Started","Grid"]}}}};export{o as default};
+//# sourceMappingURL=preview-75c64051.js.map
