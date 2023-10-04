7090cf0809c6e2d906fd36ae2a7d8e8cd8d71d4d
Maximilian <github@maximilian-heidenreich.de>
2023-10-04 11:51:51 +0200

    add min/max Size clamping to resizable

--- src/lib/Resizable.svelte
+++ src/lib/Resizable.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import { createEventDispatcher, getContext } from "svelte";
-  import type { Vec2 } from "./types/Utils.type.js";
-  import { hasClassOrParentWithClass, snapToGrid } from "./utils.js";
+  import { clamp, hasClassOrParentWithClass, snapToGrid } from "./utils.js";
   import type { Writable } from "svelte/store";
   import type { IBoard, IBoardSettings } from "./types/Board.type.js";
   import type { IPositionable } from "./Positionable.svelte";
+  import type { Vec2 } from "./index.js";
 
   export let positionable: IPositionable;
   export let direction: "wn" | "ne" | "es" | "sw";
+  export let minSize: Vec2<number> = { x: 0, y: 0 };
+  export let maxSize: Vec2<number> = { x: Infinity, y: Infinity };
+  // todo: aspect-ratio css override? (height unset)
 
   const board = getContext<IBoard>("board");
   const settings = getContext<Writable<IBoardSettings>>("settings");
@@ -23,6 +26,14 @@
     offset: { x: 0, y: 0 }
   };
 
+  // Utils
+  function clampSize(size: Vec2<number>) {
+    return {
+      x: clamp(size.x, minSize.x, maxSize.x),
+      y: clamp(size.y, minSize.y, maxSize.y)
+    };
+  }
+
   // UI Handlers
   function onMouseDown(e: MouseEvent) {
     if (hasClassOrParentWithClass(e.target as HTMLElement, "tela-ignore")) return;
@@ -32,8 +43,8 @@
     let cY = e.clientY;
     // todo: handle touch
 
-    const x = $settings.SNAP_TO_GRID ? snapToGrid(cX, $settings.GRID_SIZE!) : cX;
-    const y = $settings.SNAP_TO_GRID ? snapToGrid(cY, $settings.GRID_SIZE!) : cY;
+    const x = cX//$settings.SNAP_TO_GRID ? snapToGrid(cX, $settings.GRID_SIZE!) : cX;
+    const y = cY//$settings.SNAP_TO_GRID ? snapToGrid(cY, $settings.GRID_SIZE!) : cY;
 
     resizeState.init = { x, y };
     resizeState.curr = { x, y };
@@ -79,6 +90,10 @@
       positionable.posY += resizeState.offset.y;
     }
 
+    const clamped = clampSize({ x: positionable.width, y: positionable.height });
+    positionable.width = clamped.x;
+    positionable.height = clamped.y;
+
       htmlEl.dispatchEvent(
         new CustomEvent("resizable_change", {
           detail: { positionable },
@@ -89,7 +104,21 @@
     }
 
   function onMouseUp(e: MouseEvent) {
-    $state.mode = "draw";
+    $state.mode = "draw"; // todo: fix
+
+    const clamped = clampSize({ x: positionable.width, y: positionable.height });
+    positionable.width = $settings.SNAP_TO_GRID ? snapToGrid(clamped.x, $settings.GRID_SIZE!) : clamped.x;
+    positionable.height = $settings.SNAP_TO_GRID ? snapToGrid(clamped.y, $settings.GRID_SIZE!) : clamped.y;
+
+    // positionable.width = clamped.x;
+    // positionable.height = clamped.y;
+
+    htmlEl.dispatchEvent(
+        new CustomEvent("resizable_move_end", {
+          detail: { positionable },
+          bubbles: true
+        })
+      );
     document.removeEventListener("mousemove", onMouseMove);
     htmlEl.dispatchEvent(
         new CustomEvent("resizable_change", {
